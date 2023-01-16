dbc9fffcbc78cb2228cf2276e9d09f8669db9783
shadman sakib khan <shadman@shadmans-Mac-mini.local>
2023-01-17 03:26:16 +0600

    next init

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            // Text(
+            //   "Welcome",
+            //   style: TextStyle(
+            //     fontSize: 22,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // )
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("hi there");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+
+        // child: Center(
+        //   child: Text(
+        //     "login page",
+        //     style: TextStyle(
+        //         fontFamily: "Helvetica Neue",
+        //         fontSize: 14,
+        //         color: Colors.blue,
+        //         fontWeight: FontWeight.bold),
+        //   ),
+        // ),
+        );
+  }
+}
